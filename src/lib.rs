6d9a30f0fc984c03555c64369ff8a5b1206b97ec
Valtteri Peltola <valtteri.peltola@gmail.com>
2023-04-21 16:27:42 +0100

    use a cache api to cache results

--- src/lib.rs
+++ src/lib.rs
@@ -16,56 +16,53 @@ fn log_request(req: &Request) {
     );
 }
 
+async fn fetch_business_by_id(business_id: &str) -> Result<String> {
+    // Does it make more sense to get reqwest client as a parameter?
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{}{}", "https://avoindata.prh.fi/bis/v1/", business_id))
+        .send()
+        .await;
+
+    // How to handle errors here?
+    let api_response = match response {
+        Ok(res) => res.text().await.unwrap(),
+        Err(_) => "No company found with business id".to_string(),
+    };
+
+    return Ok(api_response);
+}
+
+fn map_api_response_to_business(api_response: String) -> Business {
+    let api_response: ApiResponse = serde_json::from_str(&api_response).unwrap();
+    let business = api_response.results[0].clone();
+
+    return business;
+}
+
 
 async fn get_business_by_business_id_from_kv_or_fetch(business_id: &str, kv: &kv::KvStore) -> Result<Business> {
     let api_response = match kv.get(business_id).text().await? {
         Some(cache_value) => cache_value,
         None => {
-            // Does it make more sense to get reqwest client as a parameter?
-            let client = reqwest::Client::new();
-
-            let response = client
-                .get(format!("{}{}", "https://avoindata.prh.fi/bis/v1/", business_id))
-                .send()
-                .await;
-            
-            // How to handle errors here?
-            let api_response = match response {
-                Ok(res) => res.text().await.unwrap(),
-                Err(_) => "No company found with business id".to_string(),
-            };
+            let api_response = fetch_business_by_id(business_id).await?;
 
             const ONE_DAY_SECONDS: u64 = 86400;
-
             kv.put(business_id, api_response.clone())?.expiration_ttl(ONE_DAY_SECONDS).execute().await?;
             
             api_response
         }
     };
 
-    let api_response: ApiResponse = serde_json::from_str(&api_response)?;
-    
-    let business = match api_response.results.first() {
-        Some(business) => business.clone(),
-        None => Business {
-            businessId: "No company found with business id".to_string(),
-            name: "No company found with business id".to_string(),
-            registrationDate: "No company found with business id".to_string(),
-            companyForm: "No company found with business id".to_string(),
-            detailsUri: None,
-            liquidations: vec![],
-            names: vec![],
-            auxiliaryNames: vec![],
-            addresses: vec![],
-            companyForms: vec![],
-            businessLines: vec![],
-            languages: vec![],
-            registedOffices: vec![],
-            contactDetails: vec![],
-            registeredEntries: vec![],
-            businessIdChanges: vec![]
-        },
-    };
+    let business = map_api_response_to_business(api_response);
+
+    Ok(business)
+}
+
+async fn get_business_by_business_id(business_id: &str) -> Result<Business> {
+    let api_response = fetch_business_by_id(business_id).await?;
+    let business = map_api_response_to_business(api_response);
 
     Ok(business)
 }
@@ -73,6 +70,7 @@ async fn get_business_by_business_id_from_kv_or_fetch(business_id: &str, kv: &kv
 fn map_body_to_response(body: String) -> Result<Response> {
     let mut headers = Headers::new();
     headers.set("Content-Type", "application/json")?;
+    headers.set("Cache-Control", "s-maxage=60")?;
 
     let response = Response::ok(body)?;
 
@@ -89,7 +87,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
 
     router
         .get("/", |_, _| Response::ok("Hello business pulse user!"))
-        .get_async("/business/:business_id", |_, ctx| async move {
+        .get_async("/v1/business/:business_id", |_, ctx| async move {
             let business_id = match ctx.param("business_id") {
                 Some(business_id) => business_id,
                 None => return Response::ok("No business id provided"),
@@ -100,6 +98,29 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             let business = get_business_by_business_id_from_kv_or_fetch(business_id, &kv).await?;
 
             map_body_to_response(serde_json::to_string(&business)?)
+        })
+        .get_async("/v2/business/:business_id", |req, ctx| async move {
+            let business_id = match ctx.param("business_id") {
+                Some(business_id) => business_id,
+                None => return Response::ok("No business id provided"),
+            };
+
+            let cache = Cache::default();
+            let cache_key = req.url()?.to_string();
+
+            if let Some(resp) = cache.get(&cache_key, true).await? {
+                console_log!("Cache HIT!");
+                Ok(resp)
+            } else {
+                console_log!("Cache MISS!");
+                let business = get_business_by_business_id(business_id).await?;
+                let mut response = map_body_to_response(serde_json::to_string(&business)?)?;
+
+                cache.put(&cache_key, response.cloned()?).await?;
+                
+                Ok(response)
+            }
+
         })
         .run(req, env)
         .await
--- src/model.rs
+++ src/model.rs
@@ -1,135 +1,146 @@
 use std::option::Option;
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct ApiResponse {
     pub r#type: String,
     pub version: String,
-    pub totalResults: i32,
-    pub resultsFrom: u32,
-    pub previousResultsUri: Option<String>,
-    pub nextResultsUri: Option<String>,
-    pub exceptionNoticeUri: Option<String>,
+    pub total_results: i32,
+    pub results_from: u32,
+    pub previous_results_uri: Option<String>,
+    pub next_results_uri: Option<String>,
+    pub exception_notice_uri: Option<String>,
     pub results: Vec<Business>,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct Business {
-    pub businessId: String,
+    pub business_id: String,
     pub name: String,
-    pub registrationDate: String,
-    pub companyForm: String,
-    pub detailsUri: Option<String>,
+    pub registration_date: String,
+    pub company_form: String,
+    pub details_uri: Option<String>,
     pub liquidations: Vec<String>,
     pub names: Vec<Name>,
-    pub auxiliaryNames: Vec<AuxiliaryName>,
+    pub auxiliary_names: Vec<AuxiliaryName>,
     pub addresses: Vec<Address>,
-    pub companyForms: Vec<CompanyForm>,
-    pub businessLines: Vec<BusinessLine>,
+    pub company_forms: Vec<CompanyForm>,
+    pub business_lines: Vec<BusinessLine>,
     pub languages: Vec<Language>,
-    pub registedOffices: Vec<RegistedOffice>,
-    pub contactDetails: Vec<ContactDetail>,
-    pub registeredEntries: Vec<RegisteredEntry>,
-    pub businessIdChanges: Vec<String>,
+    pub registed_offices: Vec<RegistedOffice>,
+    pub contact_details: Vec<ContactDetail>,
+    pub registered_entries: Vec<RegisteredEntry>,
+    pub business_id_changes: Vec<String>,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct Name {
     order: u32,
     version: u32,
     name: String,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct AuxiliaryName {
     order: u32,
     version: u32,
     name: String,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct Address {
-    careOf: Option<String>,
+    care_of: Option<String>,
     street: String,
-    postCode: String,
+    post_code: String,
     r#type: u32,
     version: u32,
     city: String,
     country: Option<String>,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     language: String,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct CompanyForm {
     version: u32,
     name: String,
     r#type: Option<String>,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     language: String,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct BusinessLine {
     order: u32,
     version: u32,
     code: String,
     name: String,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     language: String,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct Language {
     version: u32,
     name: String,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     language: String,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct RegistedOffice {
     order: u32,
     version: u32,
     name: String,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     language: String,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct ContactDetail {
     version: u32,
     value: String,
     r#type: String,
-    registrationDate: String,
-    endDate: Option<String>,
+    registration_date: String,
+    end_date: Option<String>,
     language: String,
     source: u32,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
 pub struct RegisteredEntry {
     authority: u32,
     register: u32,
     status: u32,
-    registrationDate: String,
-    endDate: Option<String>,
-    statusDate: String,
+    registration_date: String,
+    end_date: Option<String>,
+    status_date: String,
     language: String,
     description: String,
 }
